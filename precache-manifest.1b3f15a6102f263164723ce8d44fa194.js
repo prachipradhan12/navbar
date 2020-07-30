5bbcd30b7c23c1ca5be74973373193b5b6593c60
prachipradhan12 <pradhan.prachi10@gmail.com>
2020-07-30 15:34:01 +0530

    Updates

---
+++ precache-manifest.1b3f15a6102f263164723ce8d44fa194.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c31c31d5b69ef6b954d470906d49523f",
+    "url": "/navbar/index.html"
+  },
+  {
+    "revision": "76e1918a3f14789dcf0d",
+    "url": "/navbar/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "74157dd7293eb466192d",
+    "url": "/navbar/static/js/2.55fbd3b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/navbar/static/js/2.55fbd3b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76e1918a3f14789dcf0d",
+    "url": "/navbar/static/js/main.443656b5.chunk.js"
+  },
+  {
+    "revision": "216cf44fe1d4aadff1ff",
+    "url": "/navbar/static/js/runtime-main.65b1be5c.js"
+  }
+]);
